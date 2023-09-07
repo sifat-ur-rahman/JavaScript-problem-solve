045869a4153be023f413990db2d4c42e8afa29f5
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-07 19:00:57 +0600

    add

---
+++ factorial.js
@@ -0,0 +1,15 @@
+// Problem: Calculate the factorial of a number.
+
+function factorial(num) {
+    if (num === 0 || num === 1) {
+      return 1;
+    }
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  
+  console.log(factorial(5)); // Output: 120
+  
