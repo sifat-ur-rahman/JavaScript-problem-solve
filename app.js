79dddf2b688fedd2a036124682ae074a8b42c98f
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-07 08:40:52 +0600

    jsp3

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//--------------------------------------------
 // problem: 1 
 
 // Remove falsy values from array 
@@ -7,10 +8,24 @@ let array = ['a',false,'b',NaN, 0, undefined, 'c',null,'','d']
 let newArray=array.filter(Boolean)
 // console.log(newArray)
 
-
+//-------------------------------------------
 // problem: 2 
 
 //Convert any value to boolean(true or false)
 
 let value = 'hero'
-console.log(!!value)
+// console.log(!!value)
+
+
+
+//-------------------------------------------
+// problem: 3 
+
+//Resizing any array 
+let letter = ['a','b','c','d','e']
+letter.length = 3
+// console.log(letter)
+
+
+//-------------------------------------------
+// problem: 4 
